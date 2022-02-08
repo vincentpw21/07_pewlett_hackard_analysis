61b912e0db69cd9e0e8fa8646978465127a00b7a
Winting <winting.vincent@mayo.edu>
2022-02-07 16:06:33 -0800

    saved quries from 7_3_1 module)

---
+++ queries.sql
@@ -0,0 +1,20 @@
+-- retirement eligibility
+select first_name, last_name
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
+
+-- count retirement eligibility
+select count(first_name)
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
+
+-- retirement eligibility into new table
+select first_name, last_name
+into retirement_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
+
+select * from retirement_info; 
