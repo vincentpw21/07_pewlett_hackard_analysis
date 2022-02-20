ac33c147764d0ad4277c6e7694c50e81c1c62352
Winting <winting.vincent@mayo.edu>
2022-02-19 16:07:45 -0800

    update with additional small queries

--- queries/Employee_Database_Challenge.sql
+++ queries/Employee_Database_Challenge.sql
@@ -24,6 +24,8 @@ ORDER BY emp_no ASC, from_date DESC;
 SELECT *
 FROM unique_titles;
 
+DROP TABLE retiring_titles;
+
 -- number of employees by recent job title who are about to retire
 SELECT COUNT(title), title
 INTO retiring_titles
@@ -51,3 +53,51 @@ ORDER BY emp_no ASC, titles.from_date DESC;
 -- double check mentorship eligibility table
 SELECT *
 FROM mentorship_elligibility_list;
+
+-- SUMMARY ANALYSIS QUERIES
+-- mentorship available list by title for comparison
+SELECT COUNT(title), title
+INTO mentorship_list_by_title
+FROM mentorship_elligibility_list
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+-- double check mentorship by title list
+SELECT *
+FROM mentorship_list_by_title;
+
+SELECT *
+FROM dept_emp;
+
+-- total employees by title
+SELECT DISTINCT ON (emp_no) employees.emp_no, employees.first_name, employees.last_name, employees.birth_date, dept_emp.from_date, dept_emp.to_date, titles.title
+INTO employee_roster
+FROM employees
+INNER JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+INNER JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE dept_emp.to_date = '9999-01-01'
+ORDER BY emp_no ASC, titles.from_date DESC;
+-- double check table
+SELECT *
+FROM employee_roster;
+-- segment by title
+SELECT COUNT(title), title
+INTO employees_roster_by_title
+FROM employee_roster
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+-- double check table
+SELECT *
+FROM employees_roster_by_title;
+
+-- retiring first names for HR prizes
+SELECT COUNT(first_name), first_name
+INTO retiring_first_names
+FROM unique_titles
+GROUP BY first_name
+ORDER BY COUNT(first_name) DESC;
+
+SELECT *
+FROM retiring_first_names;
